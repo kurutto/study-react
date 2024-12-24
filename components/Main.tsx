044fc8380b1e22063af05b12a467beba25b2703b
kurutto <namihojo@gmail.com>
2024-12-24 16:50:32 +0900

    ♻️ メイン部分のコンポーネント化

---
+++ components/Main.tsx
@@ -0,0 +1,16 @@
+import styles from "@/styles/Home.module.css";
+import { Links } from "@/components/Links";
+import { Headline } from "@/components/Headline";
+type MainProps = {
+  page: string;
+};
+export function Main({ page }: MainProps) {
+  return (
+    <main className={styles.main}>
+      <Headline page={page}>
+        <code>pages/{page}.tsx</code>
+      </Headline>
+      <Links />
+    </main>
+  );
+}
