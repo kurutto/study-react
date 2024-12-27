2ccaddc987f223984ace432cdfb022c09e80f58e
kurutto <namihojo@gmail.com>
2024-12-28 08:19:00 +0900

    💄 .pageのスタイルの関係で表示が崩れたためMainにpropsのchildrenを追加

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,19 @@
 import classes from "src/components/Main/Main.module.css";
 import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
+import { ReactNode } from "react";
 type MainProps = {
   page: string;
+  children:ReactNode
 };
-export function Main({ page }: MainProps) {
+export function Main({ page,children }: MainProps) {
   return (
     <main className={classes.main}>
       <Headline page={page}>
         <code>pages/{page}.tsx</code>
       </Headline>
       <Links />
+      {children}
     </main>
   );
 }
